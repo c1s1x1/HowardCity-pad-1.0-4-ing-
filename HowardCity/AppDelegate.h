0b8def1e194300d32195b470d80ef8b85b05b563
紫月 <PurpleMoon@ziyuedeMacBook-Air.local>
2016-04-15 15:25:25 +0800

    Initial Commit

---
+++ HowardCity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HowardCity
+//
+//  Created by 紫月 on 16/4/15.
+//  Copyright (c) 2016年 CSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
